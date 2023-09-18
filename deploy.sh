43643f2e9fcf39cf00eafa5ac5a414de43917eee
Kuratajr <thoailata1000@gmail.com>
2023-09-19 03:32:54 +0700

    up

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-ffmpeg -nostdin -hide_banner -re -i rtsp://admin:Emngu125@kuratajr.gotdns.ch:620/onvif2 -t 20 -c:v libx264 -vf scale=1280:720 output.mp4
+ffmpeg -nostdin -hide_banner -re -i rtsp://admin:Emngu125@kuratajr.gotdns.ch:620/onvif2 -t 20 -c:v libx264 -vf scale=1280:720 output.avi
