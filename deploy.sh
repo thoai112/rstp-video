a8c88e485c381a92c276d5b318afffd9221f7af3
Kuratajr <thoailata1000@gmail.com>
2023-09-19 03:23:52 +0700

    up

--- deploy.sh
+++ deploy.sh
@@ -1,15 +1,3 @@
 #!/usr/bin/env bash
 
-URL='rtsp://admin:Emngu125@kuratajr.gotdns.ch:620/onvif2'
-
-PREFIX=/media/ggdrive/Camera/$(date "+%y%m%d")
-
-
-TIME=310 # seconds
-TIMEOUT=$(( TIME+5 ))
-
-
-FILE=$(date "+%y%m%d-%H%M%S")-imou1.mkv
-
-
-ffmpeg -i "$URL" -c:v libx264 -vf scale=1280:720 $FILE
+ffmpeg -i rtsp://admin:Emngu125@kuratajr.gotdns.ch:620/onvif2 -c:v libx264 -vf scale=1280:720 output.mp4
